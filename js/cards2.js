1cd45ce5b85149b93eb3ab6742adda68b0e4ca7f
ADAM ANDREASON <adamandreason@gmail.com>
2019-11-18 01:32:39 -0700

    testing superheroes api,

--- js/cards2.js
+++ js/cards2.js
@@ -1,22 +1,50 @@
-//https://api.jikan.moe/v3/anime/{id}/{request}/{parameter}
-
-var request = new XMLHttpRequest();
-
-request.open('GET', 'https://api.jikan.moe/v3/anime/5114');
-
-request.onreadystatechange = function () {
-  if (this.readyState === 4) {
-    console.log('Status:', this.status);
-    console.log('Headers:', this.getAllResponseHeaders());
-    console.log('Body:', this.responseText);
+async function getAPIData(url) {
+  try{
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+  } catch(error) {
+    console.error(error)
   }
-};
-
-request.send();
-console.log(request)
+}
 
 let mainArea = document.querySelector("main");
 let mainHeader = document.querySelector("h1");
+let id = getHeroNumber()
+const theData = getAPIData(`https://superheroapi.com/api/2714173338605692/${id}`)
+.then(data => {
+  for ( const heroVillain of data.results){
+      getAPIData(heroVillain.url)
+      .then(heroData => {
+          populateDOM(heroData)
+      })
+  }
+})
+
+
+
+function populateDOM(single_hero) {
+  let heroDiv = document.createElement("div")
+  let name = document.createElement("h3")
+  let pic = document.createElement('img')
+    //pokeDiv.setAttribute('class', 'charDivs')
+  //pic.setAttribute('class', 'images')
+  //cardBackStuff(pokeBack, single_pokemon)
+  let pokeNum = getPokeNumber(single_hero.id)
+  name.textContent = single_hero.name
+  pic.src = `..images/${pokeNum}.png`
+
+  pokeDiv.appendChild(name)
+  pokeDiv.appendChild(pic)
+
+  mainArea.appendChild(pokeDiv)
+
+}
+
+function getHeroNumber(id) {
+  
+
+}
 
 let testDiv = document.createElement("div")
 let testTitle = document.createElement("h1")
