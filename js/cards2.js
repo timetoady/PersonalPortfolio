a06bb112d49fa56db44be6f9bbe04c5a0a769428
ADAM ANDREASON <adamandreason@gmail.com>
2019-11-19 23:59:22 -0700

    Card creation, filters set. Lookin good.

--- js/cards2.js
+++ js/cards2.js
@@ -24,27 +24,54 @@ request.onload = function() {
   var data = JSON.parse(this.response);
   if (this.readyState === 4) {
     console.log("Status:", this.status);
+    //filter for favorite heroes and villains
+    const favs = data.filter(
+      id =>
+        id.name === "Wolverine" ||
+        id.name === "Goku" ||
+        id.name === "Superman" ||
+        id.name === "Yoda" ||
+        id.name === "One Punch Man" ||
+        id.slug === "70-batman"||
+        id.name === "Thanos" ||
+        id.slug === "149-captain-america" ||
+        id.name === "Bane" ||
+        id.slug === "98-black-canary" ||
+        id.name === "Black Lightning" ||
+        id.name === "Darth Vader" ||
+        id.name === "Data" ||
+        id.name === "Deadpool" ||
+        id.name === "Deathstroke" ||
+        id.name === "Spider-Man" ||
+        id.name === "Carnage" ||
+        id.name === "Venom" ||
+        id.name === "Chuck Norris" ||
+        id.name === "Joker" ||
+        id.name === "Cyclops" ||
+        id.name === "Daredevil" ||
+        id.name === "Doctor Doom" ||
+        id.name === "Leonardo" ||
+        id.name === "Lex Luthor"
 
-    //console.log(this.responseText);
+    );
 
-    //console.log(Object.keys(data))
-
-    data.forEach(id => {
+    favs.forEach(id => {
       //Card setup
       let theScene = document.createElement("div");
       let theCard = document.createElement("div");
       let theCardFront = document.createElement("div");
+      let cardFrontText = document.createElement("div");
       let theCardBack = document.createElement("div");
       let name = document.createElement("h2");
       let heading = document.createElement("h3");
       let stats = document.createElement("p");
       let getStats = jsonFilter(id.powerstats);
       let pic = document.createElement("img");
-      let figdiv = document.createElement("figure");
-      let imgfig = document.createElement("figcaption");
+      pic.setAttribute("class", "front_image");
       theScene.setAttribute("class", "scene");
       theCard.setAttribute("class", "card");
       theCardFront.setAttribute("class", "card__face card__face--front");
+      cardFrontText.setAttribute("class", "card__face card__face--front");
       theCardBack.setAttribute("class", "card__face card__face--back");
       theCard.addEventListener("click", function() {
         theCard.classList.toggle("is-flipped");
@@ -54,8 +81,8 @@ request.onload = function() {
       theCard.appendChild(theCardFront);
       theCard.appendChild(theCardBack);
       theCardFront.appendChild(pic);
-      figdiv.appendChild(imgfig);
-      imgfig.appendChild(name);
+      theCardFront.appendChild(cardFrontText);
+      theCardBack.appendChild(name);
       theCardBack.appendChild(heading);
       theCardBack.appendChild(stats);
       //Content
@@ -65,15 +92,10 @@ request.onload = function() {
       stats.innerText = getStats;
     });
   }
-  // document.querySelector("#addHero").addEventListener("click", () => {
-  //   let addHero = prompt("Name your Hero or Villain: ");
-
-  //   populateDOM(addHero);
-  // });
 };
 
 request.send();
-
+//JSON filter to make Powerstats appear nicely
 function jsonFilter(json) {
   return JSON.stringify(json)
     .replace(new RegExp('"', "g"), "")
@@ -88,7 +110,7 @@ function jsonFilter(json) {
     .replace(new RegExp(",", "g"), "\n");
 }
 
-//call dome to make customer hero
+//call dom to make customer hero
 function populateDOM(heroName, heroPic, heroStats) {
   let theScene = document.createElement("div");
   let theCard = document.createElement("div");
@@ -123,6 +145,7 @@ function populateDOM(heroName, heroPic, heroStats) {
   //stats.innerText = heroStats;
 }
 
+//button info to make custome hero
 document.querySelector("#addHero").addEventListener("click", () => {
   let heroName = prompt(
     "Provide the name of the hero or villain you want to add: "
@@ -132,9 +155,9 @@ document.querySelector("#addHero").addEventListener("click", () => {
     "Give your hero the following stats, seperated by commas, 1-100: INT,STR,SPD,DUR,POW,COM"
   );
   let joinedStats = rawStats.split(",");
-  console.log(joinedStats);
+
   let heroStats = document.createElement("p");
-  heroStats.setAttribute('class', 'customStats')
+  heroStats.setAttribute("class", "customStats");
   heroStats.innerText = `Intelligence: ${joinedStats[0]}\n
                           Strength: ${joinedStats[1]}\n
                           Speed: ${joinedStats[2]}\n
@@ -143,11 +166,3 @@ document.querySelector("#addHero").addEventListener("click", () => {
                           Combat: ${joinedStats[5]}\n`;
   populateDOM(heroName, heroPic, heroStats);
 });
-
-function getKeyByValue(object, value) {
-  for (var prop in object) {
-    if (object.hasOwnProperty(prop)) {
-      if (object[prop] === value) return prop;
-    }
-  }
-}
